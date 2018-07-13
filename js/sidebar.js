44652ba8a8fb632bdfdce013930a2a9ae57b7956
sriram <sriram@srirams-Mac.local>
2018-07-13 07:54:04 -0700

    modal reconfigured

--- js/sidebar.js
+++ js/sidebar.js
@@ -16,11 +16,14 @@ $(document).ready(function(){
   $('.coupon-box a').click(function(){
     $('.coup-act-modal').addClass('fire');
     $('.supreme-container').addClass('is-blurred');
+    $('.supreme-container').addClass('in-active');
   });
 
   $('.cross-wrapper-2 .fa-times').click(function(){
     $('.coup-act-modal').removeClass('fire');
     $('.supreme-container').removeClass('is-blurred');
+    $('.supreme-container').removeClass('in-active');
+
   });
 
 });
