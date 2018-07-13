72f672210accb80d02cc0b5067bfe30dbc5d4498
sriram <sriram@srirams-Mac.local>
2018-07-13 07:46:52 -0700

    added modal

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,11 +2,25 @@ $(document).ready(function(){
   $('.hamburger-wrapper a').click(function(){
 
     $('.sidebar').toggleClass('active');
+
+
   });
 
 
   $('.cross').click(function(){
     $('.sidebar').removeClass('active');
+
+  });
+
+
+  $('.coupon-box a').click(function(){
+    $('.coup-act-modal').addClass('fire');
+    $('.supreme-container').addClass('is-blurred');
+  });
+
+  $('.cross-wrapper-2 .fa-times').click(function(){
+    $('.coup-act-modal').removeClass('fire');
+    $('.supreme-container').removeClass('is-blurred');
   });
 
 });
